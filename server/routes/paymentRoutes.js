2358d58a49e21b465399888e456b088427e08a39
Dnnam1404 <duongnhutnam@gmail.com>
2025-04-22 16:12:45 +0700

    imagify

---
+++ server/routes/paymentRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  paymentPaypal,
+  paymentSuccess,
+  //paymentCancel,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post("/paypal/payment", paymentPaypal);
+router.get("/paypal/success", paymentSuccess);
+
+export default router;
